1e7ec0868e1914300c5cb0350fe8f44fa3fc9e27
ovuceljic-pk <ovuceljic@parsonskellogg.com>
2026-09-21 22:43:44 +0200

    treeq 0.4.0

--- Formula/treeq.rb
+++ Formula/treeq.rb
@@ -1,8 +1,8 @@
 class Treeq < Formula
   desc "Keyboard-driven tree view for JSON and XML, in your terminal"
   homepage "https://github.com/ognjen-vuceljic/treeq"
-  url "https://github.com/ognjen-vuceljic/treeq/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "e6c8f2124a653574df94dd9ea4227fbe3e67205b10ea3e11ec5f43bac24184fd"
+  url "https://github.com/ognjen-vuceljic/treeq/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "248adb91646b51ace2f911e72c783a3aca59a35a18576bd28987cbb01436bb3b"
   license "MIT"
   head "https://github.com/ognjen-vuceljic/treeq.git", branch: "main"
 
